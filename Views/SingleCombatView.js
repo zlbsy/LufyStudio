f693bb28d66205039262fa75b4f337b20cad7331
lubin.zhang <lubin.zhang@enish.jp>
2015-07-01 22:31:49 +0900

    singlecombat

--- Views/SingleCombatView.js
+++ Views/SingleCombatView.js
@@ -21,7 +21,7 @@ SingleCombatView.prototype.backLayerInit=function(){
 	var bitmapData = new LBitmapData(LMvc.datalist["singleCombatBackground"]);
 	var bitmap = new LBitmap(bitmapData);
 	bitmap.x = (LGlobal.width - bitmap.getWidth()) * 0.5;
-	bitmap.y = 200;
+	bitmap.y = 210;
 	self.backLayer.addChild(bitmap);
 };
 SingleCombatView.prototype.characterLayerInit=function(){
@@ -31,38 +31,63 @@ SingleCombatView.prototype.characterLayerInit=function(){
 	var currentCharacter = new SingleCombatCharacterView(null,self.controller.currentCharacterId,BattleCharacterSize.width,BattleCharacterSize.height);
 	currentCharacter.scaleX = currentCharacter.scaleY = 2;
 	self.characterLayer.addChild(currentCharacter);
-	currentCharacter.setCoordinate(LGlobal.width * 0.5 - 96,200 - 48);
+	currentCharacter.setCoordinate(LGlobal.width * 0.5 - 96,230 - 48);
 	currentCharacter.changeDirection(CharacterDirection.RIGHT);
-	console.log("self.characterLayerInit currentCharacter.data="+currentCharacter.data.minFace);
-	var win = new LPanel(new LBitmapData(LMvc.datalist["win05"]),130,130);
+
+	var win = new LPanel(new LBitmapData(LMvc.datalist["win05"]),220,130);
 	self.characterLayer.addChild(win);
-	var face = currentCharacter.data.minFace();
+	var face = currentCharacter.data.minFace(100);
 	face.x = face.y = 15;
 	self.characterLayer.addChild(face);
-
-	var bar = new StatusBarView(self.controller);
-	bar.y = 110;
-	var obj = {maxValue:200,currentValue:150,name:"HP",icon:"icon_hert",frontBar:"red_bar",barSize:140};
-	bar.set(obj);
-	self.characterLayer.addChild(bar);
+	
+	var name = getStrokeLabel(currentCharacter.data.name(),25,"#FFFFFF","#000000",4);
+	name.x = 120;
+	name.y = 15;
+	self.characterLayer.addChild(name);
 
 	var bar = new StatusBarView(self.controller);
 	bar.y = 130;
-	var obj = {maxValue:200,currentValue:150,name:"怒气",icon:"icon_hert",frontBar:"orange_ball",barSize:140};
+	var obj = {maxValue:200,currentValue:150,name:"HP",icon:"icon_hert",frontBar:"red_bar",barSize:200};
 	bar.set(obj);
 	self.characterLayer.addChild(bar);
 
 	var bar = new StatusBarView(self.controller);
-	bar.x = LGlobal.width - 150;
-	bar.y = 110;
-	var obj = {maxValue:200,currentValue:150,name:"HP",icon:"icon_hert",frontBar:"red_bar",barSize:140};
+	bar.y = 150;
+	var obj = {maxValue:200,currentValue:150,name:"怒气",icon:"orange_ball",frontBar:"red_bar",barSize:200};
 	bar.set(obj);
 	self.characterLayer.addChild(bar);
 	
 	
-	currentCharacter = new SingleCombatCharacterView(null,self.controller.targetCharacterId,BattleCharacterSize.width,BattleCharacterSize.height);
-	currentCharacter.scaleX = currentCharacter.scaleY = 2;
-	self.characterLayer.addChild(currentCharacter);
-	currentCharacter.setCoordinate(LGlobal.width * 0.5,200 - 48);
-	currentCharacter.changeDirection(CharacterDirection.LEFT);
+	targetCharacter = new SingleCombatCharacterView(null,self.controller.targetCharacterId,BattleCharacterSize.width,BattleCharacterSize.height);
+	targetCharacter.scaleX = targetCharacter.scaleY = 2;
+	self.characterLayer.addChild(targetCharacter);
+	targetCharacter.setCoordinate(LGlobal.width * 0.5,230 - 48);
+	targetCharacter.changeDirection(CharacterDirection.LEFT);
+	
+	var win = new LPanel(new LBitmapData(LMvc.datalist["win05"]),220,130);
+	win.x = LGlobal.width - win.getWidth();
+	self.characterLayer.addChild(win);
+	var face = targetCharacter.data.minFace(100);
+	face.x = LGlobal.width - 100 - 15;
+	face.y = 15;
+	self.characterLayer.addChild(face);
+
+	var bar = new StatusBarView(self.controller);
+	bar.y = 130;
+	var obj = {maxValue:200,currentValue:150,name:"HP",icon:"icon_hert",frontBar:"red_bar",barSize:200};
+	bar.set(obj);
+	bar.x = LGlobal.width - bar.getWidth();
+	self.characterLayer.addChild(bar);
+
+	var bar = new StatusBarView(self.controller);
+	bar.y = 150;
+	var obj = {maxValue:200,currentValue:150,name:"怒气",icon:"orange_ball",frontBar:"red_bar",barSize:200};
+	bar.set(obj);
+	bar.x = LGlobal.width - bar.getWidth();
+	self.characterLayer.addChild(bar);
+	
+	var name = getStrokeLabel(targetCharacter.data.name(),25,"#FFFFFF","#000000",4);
+	name.x = face.x - name.getWidth() - 10;
+	name.y = 15;
+	self.characterLayer.addChild(name);
 };
