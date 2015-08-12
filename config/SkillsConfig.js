69b845f6e83797ff4d9367474fd4b4b825c19543
lufy <lufy.legend@gmail.com>
2015-08-12 21:08:37 +0900

    data

--- config/SkillsConfig.js
+++ config/SkillsConfig.js
@@ -42,7 +42,7 @@ var SkillSubType = {
 };
 var SkillsData = [
 {id:1,name:"雷霆怒击",type:SkillType.ATTACK,subType:[SkillSubType.ATTACK_COUNT],attacks:[1,0.75,0.5],probability:100},
-{id:2,name:"万众一心",type:SkillType.HERT,hert:0,probability:100},
+{id:2,name:"万众一心",type:SkillType.HERT,subType:[SkillSubType.HERT_MINUS],hert:0,probability:100},
 {id:3,name:"隔山打牛",type:SkillType.ATTACK,subType:[SkillSubType.ATTACK_RECT],rects:[{x:0,y:0},{x:0,y:-1},{x:0,y:1},{x:-1,y:0},{x:1,y:0},{x:-1,y:-1},{x:1,y:1},{x:-1,y:1},{x:1,y:-1}],probability:100},
 {id:4,name:"天下无双",type:SkillType.ATTACK,subType:[SkillSubType.ATTACK_COUNT,SkillSubType.ENEMY_AID],attacks:[1,1],aids:[[StrategyType.DefenseAid],[StrategyType.DefenseAid]],probability:100},
 ];
