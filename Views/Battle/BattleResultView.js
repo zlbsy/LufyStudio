da9b847e7f432f6271c6cda9028fe0664142c6ff
lufy <lufy.legend@gmail.com>
2015-09-08 21:09:46 +0900

    result

--- Views/Battle/BattleResultView.js
+++ Views/Battle/BattleResultView.js
@@ -5,15 +5,19 @@ function BattleResultView(controller, result){
 	if(result){
 		if(controller.battleData.fromCity.seigniorCharaId() == LMvc.selectSeignorId){
 			//self change city
+			console.log("self change city");
 		}else{
 			//nothing
+			console.log("nothing");
 		}
 		self.showWin();
 	}else{
 		if(controller.battleData.fromCity.seigniorCharaId() == LMvc.selectSeignorId){
 			//nothing
+			console.log("nothing");
 		}else{
 			//enemy change city
+			console.log("enemy change city");
 		}
 		self.showFail();
 	}
@@ -45,21 +49,50 @@ BattleResultView.prototype.showFail=function(){
 };
 BattleResultView.prototype.enemyCaptiveFail=function(){
 	var self = this;
+	console.log("enemyCaptiveFail:"+self.model.enemyCaptive.length);
 	if(self.model.enemyCaptive.length == 0){
 		self.selfCaptiveFail();
 		return;
 	}
-	var chara = self.model.enemyCaptive[0];
+	var charaId = self.model.enemyCaptive[0];
+	var charaModel = CharacterModel.getChara(charaId);
 	self.model.enemyCaptive.splice(0, 1);
-	if(calculateHitrateSurrender(seigniorId, chara)){//投降
+	var seigniorId = self.controller.battleData.toCity.seigniorCharaId();
+	var leaderId = self.enemyLeader;
+	if(calculateHitrateSurrender(seigniorId, charaModel)){//投降
 		
-	}else if(calculateHitrateBehead(leaderId, chara)){//斩首
+	}else if(calculateHitrateBehead(leaderId, charaModel)){//斩首
 		
-	}else if(calculateHitrateRelease(leaderId, chara)){//释放
+	}else if(calculateHitrateRelease(leaderId, charaModel)){//释放
 		
 	}else{//俘虏
-		
+		self.enemyCaptiveShow(charaModel);
 	}
+};
+BattleResultView.prototype.enemyCaptiveShow=function(charaModel){
+	var self = this;
+	console.log("enemyCaptiveShow:"+charaModel);
+	var layer = new LSprite();
+	self.addChild(layer);
+	var windowData = new LBitmapData(LMvc.datalist["win05"]);
+	var windowPanel = getBitmap(new LPanel(windowData,340,230));
+	layer.addChild(windowPanel);
+	var msg = getStrokeLabel(String.format("{0}被敌军俘虏了!",charaModel.name()),20,"#CCCCCC","#000000",4);
+	msg.x = (windowPanel.getWidth() - msg.getWidth())*0.5;
+	msg.y = 20;
+	layer.addChild(msg);
+	var face = charaModel.minFace();
+	face.x = (windowPanel.getWidth() - 100) * 0.5;
+	face.y = 50;
+	layer.addChild(face);
+	layer.x = (LGlobal.width - windowPanel.getWidth()) * 0.5;
+	layer.y = (LGlobal.height - windowPanel.getHeight()) * 0.5;
+	layer.y = LGlobal.height;
+	var btnOk = getButton("OK",100);
+	btnOk.x = (windowPanel.getWidth() - btnOk.getWidth())*0.5;
+	btnOk.y = 160;
+	layer.addChild(btnOk);
+	LTweenLite.to(layer,0.4,{y:(LGlobal.height - windowPanel.getHeight()) * 0.5}) 
 };
 BattleResultView.prototype.selfCaptiveFail=function(){
 	var self = this;
