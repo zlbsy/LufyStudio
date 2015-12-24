5308ddcfcb51641e6eba32398d64810876f0044b
lufy <lufy.legend@gmail.com>
2015-12-24 23:13:57 +0900

    record

--- Models/RecordModel.js
+++ Models/RecordModel.js
@@ -6,6 +6,8 @@ RecordModel.prototype.construct=function(){
 };
 RecordModel.prototype.getImages=function(){
 	var list = [
+		{name:"close",path:LMvc.IMG_PATH+"component/close.png"},
+		{name:"win02",path:LMvc.IMG_PATH+"win/win02.png"},
 		{name:"win03",path:LMvc.IMG_PATH+"win/win03.png"},
 		{name:"win05",path:LMvc.IMG_PATH+"win/win05.png"}
 	];
