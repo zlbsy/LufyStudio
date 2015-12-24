45e82eb5191b777b63e9a6f0e886ba6d1f33d5fc
lufy <lufy.legend@gmail.com>
2015-12-24 22:33:43 +0900

    record save read

--- Views/Record/RecordChildView.js
+++ Views/Record/RecordChildView.js
@@ -7,27 +7,55 @@ function RecordChildView(index){
 	self.set(self.record);
 }
 RecordChildView.prototype.init=function(){
-	var self = this;console.log("recordChild init ",LMvc.datalist["win03"]);
-	self.backLayer = new LPanel(new LBitmapData(LMvc.datalist["win03"]), LGlobal.width - 40, 100);
+	var self = this;
+	self.backLayer = new LPanel(new LBitmapData(LMvc.datalist["win03"]), LGlobal.width - 140, 80);
 	self.addChild(self.backLayer);
+		var lblIndex = getStrokeLabel((self.recordIndex + 1)+".",25,"#FFFFFF","#000000",4);
+	lblIndex.x = 10;
+	lblIndex.y = 7;
+	self.backLayer.addChild(lblIndex);
+	
 	self.labelsLayer = new LSprite();
 	self.addChild(self.labelsLayer);
 };
 RecordChildView.prototype.set=function(record){
 	var self = this;
 	self.labelsLayer.removeAllChild();
+	console.log("RecordChildView.prototype.set="+self.record);
 	if(!record || !record.labels){
 		return;
 	}
 	var labels = record.labels;
-	var name = getStrokeLabel(labels.name,25,"#FFFFFF","#000000",4);
-	name.x = 20;
-	name.y = 20;
+	
+	var name = getStrokeLabel(labels.name+labels.name,22,"#FFFFFF","#000000",4);
+	name.x = 50;
+	name.y = 10;
 	self.labelsLayer.addChild(name);
+	
+	var cityCount = getStrokeLabel("城池 : "+labels.cityCount,20,"#FFFFFF","#000000",3);
+	cityCount.x = 50;
+	cityCount.y = 45;
+	self.labelsLayer.addChild(cityCount);
+	
+	var generalsCount = getStrokeLabel("武将 : "+labels.generalsCount,20,"#FFFFFF","#000000",3);
+	generalsCount.x = 200;
+	generalsCount.y = 45;
+	self.labelsLayer.addChild(generalsCount);
+	
+	var saveTime = getStrokeLabel(labels.saveTime,18,"#FFFFFF","#000000",2);
+	saveTime.x = 150;
+	saveTime.y = 10;
+	self.labelsLayer.addChild(saveTime);
 };
 RecordChildView.prototype.onClick=function(event){
 	var self = event.target;
-	self.record = GameManager.save(self.recordIndex);
-	self.set(self.record);
-	self.updateView();
+	console.log("mode="+RecordController.instance().mode);
+	if(RecordController.instance().mode == RecordController.SAVE_MODE){
+		self.record = GameManager.save(self.recordIndex);
+		self.set(self.record);
+		self.cacheAsBitmap(false);
+		self.updateView();
+	}else{
+	
+	}
 };
--- Views/RecordView.js
+++ Views/RecordView.js
@@ -20,19 +20,37 @@ RecordView.prototype.init=function(){
 };
 RecordView.prototype.layerInit=function(){
 	var self = this;
+	var maskBackground = getTranslucentMask();
+	self.addChild(maskBackground);
 	self.listView = new LListView();
-	self.listView.resize(LGlobal.width - 40,LGlobal.height - 100);
-	self.listView.x = 20;
+	self.listView.resize(LGlobal.width - 140,LGlobal.height - 100);
+	self.listView.x = 16;
 	self.listView.y = 80;
-	self.listView.cellWidth = LGlobal.width - 40;
-	self.listView.cellHeight = 100;
-	self.backLayer = new LPanel(new LBitmapData(LMvc.datalist["win05"]),LGlobal.width, LGlobal.height);
+	self.listView.cellWidth = LGlobal.width - 140;
+	self.listView.cellHeight = 80;
+	self.backLayer = new LPanel(new LBitmapData(LMvc.datalist["win05"]),LGlobal.width - 100, LGlobal.height);
+	self.backLayer.x = 50;
 	self.backLayer.addChild(self.listView);
 	
+	var titleData = new LBitmapData(LMvc.datalist["win02"]);
+	var titlePanel = new LPanel(titleData,200,60);
+	titlePanel.x = (LGlobal.width - titlePanel.getWidth() - 100) * 0.5;
+	self.backLayer.addChild(titlePanel);
 	var title = getStrokeLabel(self.controller.mode == RecordController.SAVE_MODE ? "保存进度":"读取进度",25,"#FFFFFF","#000000",4);
-	title.x = (LGlobal.width - title.getWidth()) * 0.5;
-	title.y = 10;
-	self.backLayer.addChild(title);
+	title.x = (titlePanel.getWidth() - title.getWidth()) * 0.5;
+	title.y = 15;
+	titlePanel.addChild(title);
 	
 	self.addChild(self.backLayer);
+	
+	var bitmapClose = new LBitmap(new LBitmapData(LMvc.datalist["close"]));
+	var buttonClose = new LButton(bitmapClose);
+	buttonClose.x = LGlobal.width - bitmapClose.getWidth() - 5;
+	buttonClose.y = 5;
+	
+	self.addChild(buttonClose);
+	buttonClose.addEventListener(LMouseEvent.MOUSE_UP,self.closeSelf);
+};
+RecordView.prototype.closeSelf=function(event){
+	RecordController.instance().hide();
 };
