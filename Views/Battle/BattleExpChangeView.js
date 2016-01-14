2efdfbea30ed86df985403f0121aebe32ca278bd
lufy <lufy.legend@gmail.com>
2016-01-14 22:59:44 +0900

    exp change

--- Views/Battle/BattleExpChangeView.js
+++ Views/Battle/BattleExpChangeView.js
@@ -16,7 +16,13 @@ function BattleExpChangeView(controller){
 BattleExpChangeView.prototype.mousedown = function(e){};
 BattleExpChangeView.prototype.mouseup = function(event){
 	var self = event.currentTarget;
-	self.setData();
+	if(self.isEnd){
+		var resultView = self.parent;
+		self.remove();
+		resultView.dispatchEvent(BattleResultEvent.CLOSE_EXP);
+	}else{
+		self.setData();
+	}
 };
 BattleExpChangeView.prototype.initLayer = function(){
 	var self = this;
@@ -27,7 +33,7 @@ BattleExpChangeView.prototype.initLayer = function(){
 BattleExpChangeView.prototype.setBackground = function(){
 	var self = this;
 	var backgroundData = new LBitmapData(LMvc.datalist["background-text01"]);
-	var panel = getBitmap(new LPanel(backgroundData,LGlobal.width,30 * (self.datas.length + 1) + 20));
+	var panel = getBitmap(new LPanel(backgroundData,LGlobal.width,30 * (self.datas.length + 2)));
 	self.baseLayer.addChildAt(panel,0);
 };
 BattleExpChangeView.prototype.initData = function(){
@@ -36,7 +42,6 @@ BattleExpChangeView.prototype.initData = function(){
 	layer.x = 50;
 	layer.y = 10;
 	self.baseLayer.addChild(layer);
-	self.expLayer = layer;
 	
 	var model = LMvc.BattleController.model;
 	var datas = [];
@@ -53,8 +58,22 @@ BattleExpChangeView.prototype.initData = function(){
 		sumExp += character.exp();
 	}
 	self.average = sumExp / datas.length;
+	var feat = self.average * 1;
+	var rewardValue = 1.5;
 	self.datas = datas.sort(function(a,b){return b.exp - a.exp;});
-	
+	for(var i=0,l=self.datas.length;i<l;i++){
+		var data = self.datas[i];
+		data.feat = feat;
+		if(data.feat < 10){
+			data.feat = 10;
+		}
+		if(data.exp >= self.average * rewardValue){
+			data.feat += feat * (data.exp / self.average  - rewardValue);
+			data.reward = true;
+		}
+		feat -= 5;
+		data.feat = data.feat >>> 0;
+	}
 	var xs = self.xs;
 	label = getStrokeLabel(Language.get("generals"),20,"#FFFFFF","#000000",4);
 	label.x = xs[0];
@@ -63,21 +82,33 @@ BattleExpChangeView.prototype.initData = function(){
 	label.x = xs[1];
 	layer.addChild(label);
 	label = getStrokeLabel(Language.get("feat"),20,"#FFFFFF","#000000",4);
-	label.x = xs[2];
+	label.x = xs[3];
 	layer.addChild(label);
+	
+	layer = new LSprite();
+	layer.x = 50;
+	layer.y = 20;
+	self.baseLayer.addChild(layer);
+	self.expLayer = layer;
 };
 BattleExpChangeView.prototype.setData = function(index){
 	var self = this, label;
-	var xs = self.xs;
-	var isAll = (typeof index == UNDEFINED);
-	if(!isAll && index >= self.datas.length){
-		return;
-	}
 	if(self.isEnd){
 		return;
 	}
+	var xs = self.xs;
+	var isAll = (typeof index == UNDEFINED);
+	if(isAll){
+		index = -1;
+		self.isEnd = true;
+		self.expLayer.removeAllChild();
+	}else if(index >= self.datas.length){
+		self.isEnd = true;
+		return;
+	}
+	
 	for(var i=0,l=self.datas.length;i<l;i++){
-		if(!isAll && i != index){
+		if(!isAll && i !== index){
 			continue;
 		}
 		var layer = new LSprite();
@@ -96,18 +127,16 @@ BattleExpChangeView.prototype.setData = function(index){
 		label = getStrokeLabel( "→",20,"#FFFFFF","#000000",4);
 		label.x = xs[2];
 		layer.addChild(label);
-		
-		var featLabel = getStrokeLabel(0,20,"#FFFFFF","#000000",4);
+		var feat = data.feat;
+		var featLabel = getStrokeLabel(isAll?feat:0,20,"#FFFFFF","#000000",4);
 		featLabel.x = xs[3];
 		featLabel.feat = 0;
 		layer.addChild(featLabel);
 		
 		var rewardLabel;
-		if(data.exp  >= self.average * 1.5){
+		if(data.reward){
 			rewardLabel = getStrokeLabel("首功",20,"#FF0000","#000000",4);
 			rewardLabel.x = xs[4];
-			rewardLabel.alpha = 0;
-			rewardLabel.scaleX = rewardLabel.scaleY = 2;
 			layer.addChild(rewardLabel);
 		}
 		if(isAll){
@@ -116,22 +145,27 @@ BattleExpChangeView.prototype.setData = function(index){
 		
 		layer.alpha = 0;
 		layer.scaleY = 0.1;
-		if(rewardLabel){
-			LTweenLite.to(layer,0.5,{alpha:1,scaleY:1,ease:LEasing.Elastic.easeOut})
-			.to(featLabel,0.5,{feat:data.exp,onUpdate:function(e){
+		if(data.reward){
+			rewardLabel.alpha = 0;
+			rewardLabel.scaleX = rewardLabel.scaleY = 2;
+			
+			LTweenLite.to(layer,0.3,{alpha:1,scaleY:1,ease:LEasing.Elastic.easeOut})
+			.to(featLabel,0.5,{feat:feat,onUpdate:function(e){
 				e.target.text = e.target.feat >>> 0;
-			}})
-			.to(rewardLabel,0.5,{alpha:1,scaleX:1,scaleY:1,
-			onComplete:function(e){
+			},onComplete:function(e){
+				e.target.text = e.target.feat >>> 0;
+			}});
+			LTweenLite.to(rewardLabel,0.3,{delay:0.8,alpha:1,scaleX:1,scaleY:1,onComplete:function(e){
 				var obj = e.target.parent;
 				var s = obj.parent.parent.parent;
 				s.setData(obj.index + 1);
 			}});
 		}else{
-			LTweenLite.to(layer,0.5,{alpha:1,scaleY:1,ease:LEasing.Elastic.easeOut})
-			.to(featLabel,0.5,{feat:data.exp,onUpdate:function(e){
+			LTweenLite.to(layer,0.3,{alpha:1,scaleY:1,ease:LEasing.Elastic.easeOut})
+			.to(featLabel,0.5,{feat:feat,onUpdate:function(e){
 				e.target.text = e.target.feat >>> 0;
 			},onComplete:function(e){
+				e.target.text = e.target.feat >>> 0;
 				var obj = e.target.parent;
 				var s = obj.parent.parent.parent;
 				s.setData(obj.index + 1);
