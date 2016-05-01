5ea56cdba76125c9fb242defce1660b3e8152665
lufy <lufy.legend@gmail.com>
2016-05-01 14:04:30 +0900

    data

---
+++ Data/03/chapter.js
@@ -0,0 +1,28 @@
+LMvc.chapterData = {
+title : "群雄逐鹿"
+,year : 194
+,month : 1
+,exp : 0, level : 1, eventList : []
+,seigniors : [
+{id:1,name:"曹操",faceImg:1,color:"0,0,255",general_count:26, citys:[40],city_count:1},
+{id:18,name:"吕布",faceImg:18,color:"1,1,1",general_count:10, citys:[39],city_count:1},
+{id:21,name:"刘备",faceImg:21,color:"0,128,0",general_count:5, citys:[35],city_count:1},
+{id:24,name:"孙策",faceImg:24,color:"255,0,0",general_count:8, citys:[29],city_count:1},
+{id:39,name:"袁绍",faceImg:39,color:"255,255,0",general_count:28, citys:[43,41,44],city_count:3},
+{id:75,name:"公孙瓒",faceImg:75,color:"0,255,0",general_count:10, citys:[46,45],city_count:2},
+{id:144,name:"公孙度",faceImg:144,color:"173,255,47",general_count:3, citys:[47],city_count:1},
+{id:160,name:"马腾",faceImg:160,color:"184,134,11",general_count:12, citys:[2,1,3],city_count:3},
+{id:237,name:"张扬",faceImg:237,color:"70,130,180",general_count:6, citys:[42],city_count:1},
+{id:333,name:"严白虎",faceImg:333,color:"102,205,170",general_count:2, citys:[31],city_count:1},
+{id:375,name:"袁术",faceImg:375,color:"128,0,0",general_count:14, citys:[26,34],city_count:2},
+{id:431,name:"刘繇",faceImg:431,color:"127,255,212",general_count:6, citys:[33],city_count:1},
+{id:446,name:"刘表",faceImg:446,color:"255,192,203",general_count:16, citys:[15,16,27],city_count:3},
+{id:451,name:"陶谦",faceImg:451,color:"135,206,235",general_count:7, citys:[36],city_count:1},
+{id:453,name:"张鲁",faceImg:453,color:"165,42,42",general_count:6, citys:[11],city_count:1},
+{id:465,name:"李傕",faceImg:465,color:"204,204,204",general_count:10, citys:[12],city_count:1},
+{id:481,name:"王朗",faceImg:481,color:"128,128,0",general_count:3, citys:[32],city_count:1},
+{id:495,name:"孔融",faceImg:495,color:"255,165,0",general_count:2, citys:[38],city_count:1},
+{id:562,name:"孔伷",faceImg:562,color:"255,160,122",general_count:2, citys:[24],city_count:1},
+{id:616,name:"刘璋",faceImg:616,color:"0,128,128",general_count:14, citys:[7,6,5],city_count:3},
+]
+};
