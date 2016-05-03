f2109aba356eb99f38d4ed1ad917bc249bf9c94e
lufy <lufy.legend@gmail.com>
2016-05-04 08:40:49 +0900

    剧本

--- Data/01/chapter.js
+++ Data/01/chapter.js
@@ -5,6 +5,6 @@ title : "黄巾之乱"
 ,exp : 0, level : 1, eventList : []
 ,seigniors : [
 {id:66,name:"张角",faceImg:66,color:"255,255,0",general_count:21, citys:[39,43,41,24,44],city_count:5},
-{id:604,name:"何进",faceImg:604,color:"1,1,1",general_count:34, citys:[14,45,23,42,22,12],city_count:6},
+{id:604,name:"何进",faceImg:604,color:"1,1,1",general_count:31, citys:[14,45,23,42,22,12],city_count:6},
 ]
 };
