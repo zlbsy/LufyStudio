f168b970edc1c2ff5df54fddb3c2f74570cf119d
lubin.zhang <lubin.zhang@enish.jp>
2016-09-14 19:16:09 +0900

    anhao

--- Android/app/src/main/java/com/lufylegend/sgj/MainActivity.java
+++ Android/app/src/main/java/com/lufylegend/sgj/MainActivity.java
@@ -1,8 +1,11 @@
 package com.lufylegend.sgj;
 
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +18,23 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("base = " + decoded);
         String dec = AESUtil.decrypt(data);
         System.out.println("decrypt = " + dec);*/
-        String src = "my test";
-        String enc = AESUtil.encrypt(src);
-        android.util.Log.e("enc",enc);
-        String dec = AESUtil.decrypt(enc);
-        android.util.Log.e("dec",dec);
+        try {
+            String text = "NGU1V2E3MWZZb1Q3TUZFWGZNKt5//YTfSVsHSmQaA0c=";
+            byte[] byteText = android.util.Base64.decode(text, 9999);
+            String ENCRYPT_KEY = "RwcmlVpg";
+            String ENCRYPT_IV = "4e5Wa71fYoT7MFEX";
+            byte[] byteKey = ENCRYPT_KEY.getBytes("UTF-8");
+            byte[] byteIv = ENCRYPT_IV.getBytes("UTF-8");
+            SecretKeySpec key = new SecretKeySpec(byteKey, "AES");
+            IvParameterSpec iv = new IvParameterSpec(byteIv);
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, key, iv);
+            byte[] byteResult = cipher.doFinal(byteText);
+            String strResult = new String(byteResult, "UTF-8");
+            android.util.Log.e("strResult", strResult);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
         Lufylegend.initialize("game/",this);
     }
 }
--- Node/run.js
+++ Node/run.js
@@ -20,28 +20,60 @@ function encrypt(data){
 var src = 'Hello,CryptWorld';
 console.log(encrypt(src));*/
 var crypto = require("crypto");
+var data = "my name is lufy";
+var key = crypto.createHash("sha256").update("RwcmlVpg").digest();
+var iv = crypto.randomBytes(16);
+console.log("iv=",typeof iv);
+var iv = '4e5Wa71fYoT7MFEX';
+var buffer = new Buffer(iv, 'utf-8');
+iv = buffer;
+console.log("key=",key.toString());
+console.log("iv=",iv.toString(), buffer.toString("utf-8"));
+var cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+var encrypted = cipher.update(data, 'utf-8');
+encrypted = Buffer.concat([iv, encrypted, cipher.final()]);
+var e = encrypted.toString('base64');
+console.log("encrypted.toString('hex') == ",e);
+//var buf = new Buffer('xl95gDHGiHnDiHrbUF+m2BTRf7BO3HibcXAhORCRatM1mfwDEVmLdDb/W4IG2lgO','base64');
+//var key = new Buffer('12345678901234567890123456789012');
+var buf = new Buffer(e,'base64');
+var iv = buf.slice(0,16);
+var encryptedData = buf.slice(16);
+var decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+var decrypted = decipher.update(encryptedData, 'base64', 'utf8');
+decrypted += decipher.final('utf8');
+console.log(decrypted);
 
-var key = "abcdefghijklmnopqrstuvwx";
-
+/*
+var key = "abcdefghijkl1234RwcmlVpg";
+console.log("key="+key);
 var pt = "hello world!";
 
-var encrypt = crypto.createCipheriv('des-ede3', key, "");
+var encrypt = crypto.createCipheriv('aes-256-cbc', key, "");
 var theCipher = encrypt.update(pt, 'utf8', 'base64');
 theCipher += encrypt.final('base64');
 console.log(theCipher);
 
-var decrypt = crypto.createDecipheriv('des-ede3', key, "");
+var decrypt = crypto.createDecipheriv('aes-256-cbc', key, "");
 var s = decrypt.update(theCipher, 'base64', 'utf8');
 console.log(s + decrypt.final('utf8'));
+
+var password ='passowrd';
+
+var sha512 = crypto.createHash('sha512');
+sha512.update(password);
+var hash = sha512.digest('hex');
+
+console.log(hash);*/
 /*
 var crypto = require('crypto');
 
 var SimpleEncrypt = (function () {
   function cipher(mode, key, iv, data) {
     var encipher = crypto[mode]('aes-256-cbc', key, iv),
-    encoded  = encipher.update(data);
-
-    encoded += encipher.final('utf8');
+    encoded  = encipher.update(data, 'utf-8', 'base64');
+	encoded = Buffer.concat([iv, encoded, encipher.final('base64')]);
+    //encoded += encipher.final('utf8');
     return encoded;
   }  
 
@@ -66,16 +98,17 @@ var SimpleEncrypt = (function () {
 }());
 
 var cryptkey   = crypto.createHash('sha256').update('Nixnogen').digest(),
-    iv         = 'a2xhcgAAAAAAAAAA',
+    iv         = crypto.randomBytes(16),
     buf        = "my test", // 32 chars
     enc        = SimpleEncrypt.encrypt(cryptkey, iv, buf);
-    dec        = SimpleEncrypt.decrypt(cryptkey, iv, enc);
+    //dec        = SimpleEncrypt.decrypt(cryptkey, iv, enc);
 
 
 console.warn("Encoded length: ", enc.length);
 console.warn("Encoded in Base64:", SimpleEncrypt.b64enc(enc));
-console.warn("Decoded:", dec);*/
+//console.warn("Decoded:"+ dec);
 //console.warn("Decoded:", dec);
+*/
 /*
  // 异步读取
  fs.readFile('input.txt', function (err, data) {
@@ -91,9 +124,10 @@ console.warn("Decoded:", dec);*/
 
  console.log("程序执行完毕。" + process.argv.length);
  */
+
 var IOS_PATH = '../Swift01/Swift01/game';
 var ANDROID_PATH = '../Android/app/src/main/assets/game1';
-
+/*
 //fs.rmdirSync(ANDROID_PATH);
 function readGameFiles(path, toPath){
 	fs.readdir(path, function(err, files) {
@@ -125,9 +159,10 @@ if(isDir( ANDROID_PATH )){
 		if (error !== null) {
 			console.log('exec error: ' + error);
 		} else {
-			readGameFiles(IOS_PATH, ANDROID_PATH);
+			//readGameFiles(IOS_PATH, ANDROID_PATH);
 		}
 	});
 }else{
-	readGameFiles(IOS_PATH, ANDROID_PATH);
+	//readGameFiles(IOS_PATH, ANDROID_PATH);
 }
+*/
